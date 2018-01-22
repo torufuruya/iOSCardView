6b3b15ca0d74d454f1ae069ca7b6debe8655c30b
Toru Furuya <t.furuya825@gmail.com>
2018-01-23 01:47:27 +0900

    Get data from API and display them

--- iOSCardView/CardViewCell.swift
+++ iOSCardView/CardViewCell.swift
@@ -13,8 +13,39 @@ class CardViewCell: UITableViewCell {
     @IBOutlet weak var view: UIView!
     @IBOutlet weak var shadowView: UIView!
 
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var publishedAt: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var kindContainerView: UIView!
+
     var numberOfRows = 0
     var indexPath: IndexPath? = nil
+    var isTappable = true
+
+    private var appData: AppData? = nil
+
+    func configure(appData: AppData) {
+        self.appData = appData
+
+        //description
+        descriptionLabel.text = appData.description
+        //amount
+        amountLabel.text = "¥\(appData.amount)"
+
+        //pushed_at
+        let f = DateFormatter()
+        f.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.000'Z'"
+        if let d = f.date(from: appData.pushed_at) {
+            f.dateFormat = "yyyy/MM/dd"
+            publishedAt.text = f.string(from: d)
+        }
+        publishedAt.isHidden = false
+
+        //kind
+        kindLabel.text = appData.kind
+        kindContainerView.isHidden = false
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -34,112 +65,89 @@ class CardViewCell: UITableViewCell {
 
     override func prepareForReuse() {
         super.prepareForReuse()
-        //Remove created mask to reuse for another cell
-        if let mask = view.layer.mask {
-            mask.removeFromSuperlayer()
-        }
+
+        descriptionLabel.text = nil
+        amountLabel.text = nil
+        publishedAt.text = nil
     }
 
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         //To draw appropriate layout first
+        kindContainerView.layer.cornerRadius = 10
+        kindContainerView.layer.borderWidth = 2
 
-        if numberOfRows == 1 {
-            makeShadowToLayer(layer: view.layer, corners: .allCorners, cornerRadius: 5)
-        } else if indexPath?.row == 0 {
-            makeShadowToLayer(layer: view.layer, corners: [.topLeft, .topRight], cornerRadius: 5)
-        } else if indexPath?.row == numberOfRows - 1 {
-            makeShadowToLayer(layer: view.layer, corners: [.bottomLeft, .bottomRight], cornerRadius: 5)
+        if appData?.kind == "capture" {
+            // capture
+            kindLabel.textColor = .blue
+            kindContainerView.backgroundColor = UIColor.blue.withAlphaComponent(0.1)
+            kindContainerView.layer.borderColor = UIColor.blue.cgColor
         } else {
-            makeShadowToLayer(layer: view.layer, corners: [], cornerRadius: 0)
+            // refund, cancel
+            kindLabel.textColor = .white
+            kindContainerView.backgroundColor = .lightGray
+            kindContainerView.layer.borderColor = UIColor.darkGray.cgColor
         }
+
+        makeCornerAndShadow()
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        if numberOfRows == 1 {
-            makeShadowToLayer(layer: view.layer, corners: .allCorners, cornerRadius: 5)
-        } else if indexPath?.row == 0 {
-            makeShadowToLayer(layer: view.layer, corners: [.topLeft, .topRight], cornerRadius: 5)
-        } else if indexPath?.row == numberOfRows - 1 {
-            makeShadowToLayer(layer: view.layer, corners: [.bottomLeft, .bottomRight], cornerRadius: 5)
-        } else {
-            makeShadowToLayer(layer: view.layer, corners: [], cornerRadius: 0)
-        }
+        makeCornerAndShadow()
     }
 
-    private func isFirstRow() -> Bool {
-        if numberOfRows == 1 || indexPath?.row == 0 {
-            return true
-        }
-        return false
-    }
+    func makeCornerAndShadow() {
+        let isSingleRow = numberOfRows == 1
+        let isFirstRow = numberOfRows == 1 || indexPath?.row == 0
+        let isLastRow = numberOfRows == 1 || indexPath?.row == numberOfRows - 1
 
-    private func isLastRow() -> Bool {
-        if numberOfRows == 1 || indexPath?.row == numberOfRows - 1 {
-            return true
-        }
-        return false
-    }
-
-    private func isSingleRow() -> Bool {
-        return numberOfRows == 1
-    }
-
-
-    private func makeShadowToLayer(layer: CALayer, corners: UIRectCorner, cornerRadius: CGFloat) {
-        // the shadow rect determines the area in which the shadow gets drawn
+        // Rect for shadow
         var shadowRect = view.bounds
-        if !isSingleRow() {
+        if !isSingleRow {
             shadowRect = shadowRect.insetBy(dx: 0, dy: -5)
-            if (isFirstRow()) { shadowRect.origin.y += 5 }
-            if (isLastRow()) { shadowRect.size.height -= 5 }
+            if (isFirstRow) { shadowRect.origin.y += 5 }
+            if (isLastRow) { shadowRect.size.height -= 5 }
         }
 
+        // Rect for corner radius mask
         var maskRect = view.bounds.insetBy(dx: -20, dy: 0)
-        if (isFirstRow()) {
+        if (isFirstRow) {
             maskRect.origin.y -= 5
             maskRect.size.height += 5
         }
-        if (isLastRow()) { maskRect.size.height += 10 }
+        if (isLastRow) { maskRect.size.height += 10 }
 
-        // now configure the background view layer with the shadow
         shadowView.layer.masksToBounds = false
         shadowView.layer.shadowColor = UIColor.black.cgColor
         shadowView.layer.shadowOffset = .zero
-        shadowView.layer.shadowRadius = 2
+        shadowView.layer.shadowRadius = isTappable ? 2 : 0.5
         shadowView.layer.shadowOpacity = 0.5
         shadowView.layer.shadowPath = UIBezierPath(roundedRect: shadowRect, cornerRadius: 5).cgPath
         shadowView.layer.shouldRasterize = true
         shadowView.layer.rasterizationScale = UIScreen.main.scale
 
-        // and finally add the shadow mask
         // Mask for shadow
         let shadowMask = CAShapeLayer()
         shadowMask.path = UIBezierPath(rect: maskRect).cgPath
         shadowView.layer.mask = shadowMask
 
         // Mask for corner radius
+        var corners: UIRectCorner = []
+        if isSingleRow { corners = [.allCorners] }
+        else if isFirstRow { corners = [.topLeft, .topRight] }
+        else if isLastRow { corners = [.bottomLeft, .bottomRight] }
+
         let cornerMask = CAShapeLayer()
         let path = UIBezierPath(roundedRect: view.bounds,
                                 byRoundingCorners: corners,
                                 cornerRadii: CGSize(width: 5, height: 5))
         cornerMask.path = path.cgPath
-        layer.mask = cornerMask
-        layer.borderColor = UIColor.black.withAlphaComponent(0.1).cgColor
-        layer.borderWidth = 0.3
-        layer.masksToBounds = false
+        view.layer.mask = cornerMask
+        view.layer.borderColor = UIColor.black.withAlphaComponent(0.1).cgColor
+        view.layer.borderWidth = 0.3
+        view.layer.masksToBounds = false
     }
-}
-
-
-struct Data : Codable {
-    let id: String
-    let amount: Int
-    let currency: String
-    let description: String
-    let kind: String
-    let pushed_at: String
 }
 
--- iOSCardView/ViewController.swift
+++ iOSCardView/ViewController.swift
@@ -15,20 +15,67 @@ class ViewController: UIViewController {
 
     private let identifier = String(describing: CardViewCell.self)
 
+    private var appData: [AppData] = []
+    private var totalAmount = 0
+
+    private let refresh = UIRefreshControl()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         //Configure table view components
         tableView.backgroundColor = .white
         tableView.register(UINib.init(nibName: identifier, bundle: Bundle.main), forCellReuseIdentifier: identifier)
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableViewAutomaticDimension
 
-        Alamofire.request("http://www.mocky.io/v2/5a275eb23000006e3c0e8a5e").responseData { response in
-            print(response)
-        }
+        refresh.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
+        refresh.attributedTitle = NSAttributedString(string: "Fetching Application Data...")
+        self.tableView.refreshControl = refresh
+
+        loadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+
+    @objc private func refresh(_ sender: Any) {
+        loadData()
+    }
+
+    private func loadData() {
+        Alamofire.request("http://www.mocky.io/v2/5a275eb23000006e3c0e8a5e").responseJSON { response in
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) { self.refresh.endRefreshing() }
+
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            guard let data = response.data else {
+                return
+            }
+            do {
+                self.appData = try decoder.decode([AppData].self, from: data)
+                self.totalAmount = self.appData.map({$0.amount}).reduce(0){$0 + $1}
+                self.tableView.reloadData()
+            } catch {
+                self.showAlert(title: "Error", message: error.localizedDescription)
+            }
+        }
+    }
+
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+struct AppData : Codable {
+    let id: String
+    let amount: Int
+    let currency: String
+    let description: String
+    let kind: String
+    let pushed_at: String
 }
 
 extension ViewController: UITableViewDataSource {
@@ -38,17 +85,32 @@ extension ViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return section == 0 ? 10 : 20
+        if appData.count > 0 {
+            return section == 0 ? appData.count : 1
+        }
+        return 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: CardViewCell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! CardViewCell
-        cell.textLabel?.text = "\(indexPath.section)-\(indexPath.row)"
-        let numberOfRows = self.tableView(tableView, numberOfRowsInSection: indexPath.section)
-        cell.numberOfRows = numberOfRows
+
+        cell.numberOfRows = tableView.numberOfRows(inSection: indexPath.section)
         cell.indexPath = indexPath
+
+        if indexPath.section == 0 {
+            let entity = appData[indexPath.row]
+            cell.configure(appData: entity)
+            cell.isTappable = true
+        } else {
+            cell.descriptionLabel?.text = "Grand Total"
+            cell.amountLabel.text = "¥\(totalAmount)"
+            cell.kindContainerView.isHidden = true
+            cell.publishedAt.isHidden = true
+            cell.isTappable = false
+        }
         //Don't do this cause it waste more memory than layouts
         //cell.setNeedsDisplay()
+
         return cell
     }
 
@@ -77,4 +139,16 @@ extension ViewController: UITableViewDataSource {
 
 extension ViewController: UITableViewDelegate {
 
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if indexPath.section == 0 {
+            return indexPath
+        }
+        //Untapptable section
+        return nil
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let entity = appData[indexPath.row]
+        showAlert(title: entity.id, message: entity.description)
+    }
 }
